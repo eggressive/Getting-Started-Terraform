9805b02d6155e204a9f9fdee89e38a25f5680d18
Dimitar Dimitrov <mitko.69@gmail.com>
2024-03-24 18:12:41 +0000

    Update load balancer configuration and outputs (final)

--- base_web_app/outputs.tf
+++ base_web_app/outputs.tf
@@ -1,9 +1,14 @@
-output "aws_instance_public_dns" {
-  value       = "http://${aws_instance.nginx1.public_dns}"
-  description = "public DNS for the instance"
+output "ALB_public_dns" {
+  value       = "http://${aws_lb.nginx.dns_name}"
+  description = "ALB public DNS"
 }
 
-output "aws_instance_public_ip" {
+output "aws_instance1_public_ip" {
   value       = aws_instance.nginx1.public_ip
-  description = "Public IP for the instance"
+  description = "Public IP for instance 1"
+}
+
+output "aws_instance2_public_ip" {
+  value       = aws_instance.nginx2.public_ip
+  description = "Public IP for instance 2"
 }
