78aa17851e009fcc113a3ffdd69c3c90bf89f9b3
Dimitar <47664474+eggressive@users.noreply.github.com>
2024-04-07 17:01:23 +0200

    Add new instance, ALB (#4)
    * Update AWS region and install nginx1
    
    * Add required providers and resources for AWS and TLS
    
    * Update load balancer configuration and outputs (final)

--- base_web_app/variables.tf
+++ base_web_app/variables.tf
@@ -2,14 +2,14 @@ variable "aws_access_key" {
   type        = string
   description = "AWS access key"
   sensitive   = true
-  default = ""
+  default     = ""
 }
 
 variable "aws_secret_key" {
   type        = string
   description = "AWS secret key"
   sensitive   = true
-  default = ""
+  default     = ""
 }
 
 variable "aws_region" {
@@ -24,10 +24,10 @@ variable "vpc_cidr_block" {
   default     = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr_block" {
-  type        = string
-  description = "CIDR block for the subnet"
-  default     = "10.0.0.0/24"
+variable "public_subnets_cidr_block" {
+  type        = list(string)
+  description = "CIDR block for public subnets in VPC"
+  default     = ["10.0.0.0/24", "10.0.1.0/24"]
 }
 
 variable "instance_type" {
