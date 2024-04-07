a45d586fb9aa54ab9201bdbf47bc4f19b1646ea0
Dimitar Dimitrov <mitko.69@gmail.com>
2024-04-07 16:41:32 +0000

    add S3 access for instances

--- base_web_app/instances.tf
+++ base_web_app/instances.tf
@@ -61,3 +61,52 @@ EOF
 
   tags = local.common_tags
 }
+
+# S3 access for instances
+resource "aws_iam_role" "allow_nginx_s3" {
+  name = "allow_nginx_s3"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy" "allow_s3_all" {
+  name = "allow_s3_all"
+  role = aws_iam_role.allow_nginx_s3.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:*",
+        ]
+        Effect = "Allow"
+        Resource = [
+          "arn:aws:s3:::${local.s3_bucket_name}",
+          "arn:aws:s3:::${local.s3_bucket_name}/*"
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "nginx_profile" {
+  name = "nginx_profile"
+  role = aws_iam_role.allow_nginx_s3.name
+
+  tags = local.common_tags
+}
--- base_web_app/providers.tf
+++ base_web_app/providers.tf
@@ -1,22 +1,9 @@
 ##################################################################################
 # PROVIDERS
 ##################################################################################
-terraform {
-  required_providers {
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5"
-    }
-  }
-}
 
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  # profile    = "tfuser"
-  region = var.aws_region
+  region     = var.aws_region
 }
